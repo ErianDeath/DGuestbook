cbd8145e49872f1c590f2248f84d5c69c1224598
Sky Summer <skysommer0317@gmail.com>
2025-09-08 15:10:38 +0800

    feat/react: re-deploy contracts

--- packages/reactApp/src/contracts/config.ts
+++ packages/reactApp/src/contracts/config.ts
@@ -2,11 +2,11 @@ import GuestBoardABI from '../abis/GuestBoard.json';
 import GuestBoardNFTABI from '../abis/GuestBoardNFT.json';
 
 export const GUESTBOARD_CONTRACT = {
-  address: '0x48c9F9526f59DA3E8F1fE5C38C996E8Ce4cbf8E4',
+  address: '0x98BAF76B397a1e16C1d24f9B2f7b688699f4095C',
   abi: GuestBoardABI.abi,
 } as const;
 
 export const GUESTBOARD_NFT_CONTRACT = {
-  address: '0x817659384A83e7022fDffb34f2baB6118cDF9C27',
+  address: '0x5194A31F134CB8F4dD903d14beFeFBad2E5277d5',
   abi: GuestBoardNFTABI.abi,
 } as const;
